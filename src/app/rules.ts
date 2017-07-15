50db39f0c09063bb3ba2e9162e44d1f2b7ad1352
Vicente Neto <sneto.vicente@gmail.com>
2017-07-15 14:53:37 -0300

    Task 1 finished

--- src/app/rules.ts
+++ src/app/rules.ts
@@ -1,6 +1,5 @@
 import {
     AngularmService, EntityLineComponent, 
-    CreateEntityComponent, EditEntityFormComponent,
     FormLineComponent, EditFormLineComponent,
     EntityDetailsComponent, ShowLineComponent,
     ListingTableComponent
@@ -17,8 +16,8 @@ export let defineRules = (angularm: AngularmService) => {
         .detr('list_entities', ListingTableComponent)
         .der('table_line', EntityLineComponent)
         .der('show_entity', EntityDetailsComponent)
-        .detr('create_form', CreateEntityComponent)
-        .der('edit_form', EditEntityFormComponent)
+        .detr('create_form', CssCreateFormComponent, { formClass: 'w3-card-4' })
+        .der('edit_form', CssEditFormComponent, { formClass: 'w3-card-4' })
         .pr('edit_form_line', '*', 'code', null, EditFormLineComponent, { inputType: 'number' })
         .dpr('edit_form_line', EditFormLineComponent, { inputType: 'text' });
 };
